651529977883ae6f2b82089f2be0ec5f9812d72c
beckyj123 <becky.j.1@live.com>
2024-03-09 18:27:55 +0800

    Started API (finish later)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "b14c254b09dec6f3a29ef3tcf98o9041";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
