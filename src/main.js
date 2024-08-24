d7eb42030200193ff0aaf3a49191bb7b118d2b02
beckyj123 <becky.j.1@live.com>
2024-08-24 18:20:54 +0800

    Completed the page

--- src/main.js
+++ src/main.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "b14c254b09dec6f3a29ef3tcf98o9041";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -63,19 +70,25 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml = forecastHtml += `
-  <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon"> ☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5)
+      forecastHtml = forecastHtml += `
+  
+    <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div class="weather-forecast-icon">  <img src="${
+      day.condition.icon_url
+    }" class="weather-forecast-icon" />
+       </div>
     <div class="weather-forecast-temperatures">
       <div class="weathe-forecast-temperature">
-        <strong>15°</strong>
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
       </div>
-      <div class="weather-forecast-temperature">9°</div>
+      <div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}</div>
     </div>
   </div>
 `;
@@ -88,4 +101,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Taipei");
-displayForecast("Taipei");
